ad389e3c212df368e89de0d78a78a7008e14de65
Naufal Akbar Nugroho <naufalakbar378@gmail.com>
2026-05-17 20:37:24 +0700

    fix: fix title

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE =
-  "Tiba Tiba Ngoding - Bangun Aplikasi Impianmu Bersama Kami";
+export const SITE_TITLE = "Tiba Tiba Ngoding";
 export const SITE_DESCRIPTION =
   "Wujudkan Aplikasi Impian Anda untuk Meningkatkan Bisnis Anda.";
